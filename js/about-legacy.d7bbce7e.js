61ba55b5fa606ebed32f7bba07da0958f703025a
fariborzemami <fariborz.emami1997@gmail.com>
2022-04-30 06:14:49 +0430

    deploy

---
+++ js/about-legacy.d7bbce7e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkbenefitcalculator"]=self["webpackChunkbenefitcalculator"]||[]).push([[443],{15830:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],l=n(1001),c={},r=(0,l.Z)(c,u,a,!1,null,null,null),s=r.exports}}]);
+//# sourceMappingURL=about-legacy.d7bbce7e.js.map
